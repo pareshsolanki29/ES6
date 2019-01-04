5647500cb642691f166a3b1ca93ce54524228b1b
pareshsolanki29 <bandralites@gmail.com>
2019-01-04 18:08:42 -0500

    add

---
+++ index.js
@@ -0,0 +1,9 @@
+let word1 = "Paresh";
+let word2 ="Solanki";
+ let num1 = 1;
+ let num2 = 2;
+  //template literal 
+ const fullNmae  = `${word1}  ${word2}`;
+  let example = `hello
+                 world`;
+                 document.getElementById("example").innerHTML = fullName + example;
